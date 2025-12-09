770be683061dcecaae9cf6cd7edb485e51b6c553
sarankumarroyal82 <sarankumarroyal82@gmail.com>
2025-12-09 11:50:23 +0530

    new class has created

---
+++ src/testng/Registation.java
@@ -0,0 +1,20 @@
+package testng;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+public class Registation {
+	WebDriver driver;
+  @Test
+  public void google() {
+	  driver.get("https://www.google.com");
+  }
+  @BeforeTest
+  public void beforeTest() {
+	  driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+  }
+
+}
